0be03f9e361f758f9b97fe5eff54cbac8e041a0e
Christina Hedges <christina.l.hedges@nasa.gov>
2018-01-12 12:34:57 -0500

    Added star integers

--- mkplot_multis.py
+++ mkplot_multis.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
+from tqdm import tqdm
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from bokeh.plotting import *
 from bokeh.models import OpenURL, Circle, HoverTool, PanTool, BoxZoomTool, ResetTool, SaveTool, TapTool, WheelZoomTool
@@ -17,10 +17,27 @@ rad = nexa.data['pl_radj'].astype(float)
 per = nexa.data['pl_orbper'].astype(float)
 teff = nexa.data['st_teff'].astype(float)
 letter = nexa.data['pl_letter'].astype(str)
+st_rad = nexa.data['st_rad'].astype(float)
 
 unq = np.unique(name, return_counts=True)[1] > 1
 mask = [n in np.unique(name)[unq] for n in name]
 
-params = [name[mask], mass[mask], rad[mask], per[mask], teff[mask], letter[mask]]
-param_names = ['Name', 'Mass', 'Radius', 'Period', 'Teff', 'Letter']
-df = pd.DataFrame(np.asarray(params).T, columns=param_names)
+params = [name[mask], mass[mask], rad[mask], per[mask], teff[mask], st_rad[mask], letter[mask]]
+param_names = ['Name', 'Mass', 'Radius', 'Period', 'StTeff', 'StRadius', 'Letter']
+
+df = pd.DataFrame()
+for param, param_name in zip(params, param_names):
+    df[param_name] = param
+
+#Drop any NaN radius value planets
+df.dropna(axis=0, subset=['Radius']).reset_index(drop=True)
+df['PeriodRatio'] = 1
+unique_names = df.Name.unique()
+for unq in tqdm(unique_names):
+    d = df[df.Name == unq]
+    df.PeriodRatio[df.Name == unq] = np.copy(d.Period.astype(float)/d.Period.min().astype(float))
+df['StarInt'] = 0
+for idx, n in enumerate(tqdm(unique_names)):
+    df.StarInt[df.Name == n] = idx
+
+df.to_csv('multiples.csv', index=False)
